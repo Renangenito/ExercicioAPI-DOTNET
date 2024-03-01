9766b77c07b707c0c0cd5c4f6adc2bf2c22580dc
renan lima <renangenito@hotmail.com>
2024-03-01 12:26:33 -0300

    Subindo o projeto completo

---
+++ ExercicioAPI/Controllers/TabuadaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TabuadaController : ControllerBase
+    {
+
+        [HttpGet("{anoNascimento}")]
+
+        public string get(int anoNascimento)
+        {
+            int idade = DateTime.Now.Year - anoNascimento;
+            
+            if (idade > 18)
+            {
+                return "Você tem "+ idade + " anos e é maior de idade";
+            }
+            else
+            {
+                return "Você tem " + idade + " anos e é menor de idade";
+            }
+        }
+    }
+}
